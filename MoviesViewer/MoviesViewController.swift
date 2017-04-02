74a5f101d5a53d4d9a705f59ccdd1a4b67cab4a6
dkumawat <diwakar_kumawat@intuit.com>
2017-04-02 15:04:49 -0700

    wip details screen

--- MoviesViewer/MoviesViewController.swift
+++ MoviesViewer/MoviesViewController.swift
@@ -15,6 +15,32 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     var movies: [NSDictionary]?
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // Get the index path from the cell that was tapped
+        let indexPath = movieTableView.indexPathForSelectedRow
+        // Get the Row of the Index Path and set as index
+        let index = indexPath?.row
+        // Get in touch with the DetailViewController
+        let detailViewController = segue.destination as! MovieDetailViewController
+        // Pass on the data to the Detail ViewController by setting it's indexPathRow value
+        
+        // get movie details
+        let movie = movies![index!]
+        let adult = movie["adult"] as! Bool
+        let releaseDate = movie["release_date"] as! String
+        let popularity = movie["popularity"] as! Float
+        let votes = movie["vote_count"] as! Float
+        
+        // pass the details
+        detailViewController.adult = adult
+        detailViewController.released = releaseDate
+        detailViewController.popularity = popularity
+        detailViewController.votes = votes
+        
+    }
+    
+    
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         if let movies = movies {
@@ -64,7 +90,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                                                                             if let responseDirectory = try! JSONSerialization.jsonObject(
                                                                                 with: data, options: []) as? NSDictionary {
                                                                                 NSLog("response: \(responseDirectory)")
-                                                                                self.movies = responseDirectory["results"] as! [NSDictionary]
+                                                                                self.movies = (responseDirectory["results"] as! [NSDictionary])
                                                                                 self.movieTableView.reloadData()
                                                                             }
                                                                         }
